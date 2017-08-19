88e661fcc01c7c9d4cbba90af2f2aab6c305a794
Clownier <Clownier.97@gmail.com>
2017-08-19 20:23:35 +0800

    537. Complex Number Multiplication

---
+++ 537_Complex_Number_Multiplication.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String complexNumberMultiply(String a, String b) {
+        int idx_a = a.indexOf('+');
+        int idx_b = b.indexOf('+');
+        idx_a = idx_a<0?a.indexOf('-'):idx_a;
+        idx_b = idx_b<0?b.indexOf('-'):idx_b;
+        int a_x = Integer.parseInt(a.substring(0,idx_a));
+        int b_x = Integer.parseInt(b.substring(0,idx_b));
+        int a_y = Integer.parseInt(a.substring(idx_a+1,a.length()-1));
+        int b_y = Integer.parseInt(b.substring(idx_b+1,b.length()-1));
+        int x = a_x*b_x - a_y*b_y;
+        int y = a_x*b_y + a_y*b_x;
+        return (x+"+"+y+"i");
+    }
+}
